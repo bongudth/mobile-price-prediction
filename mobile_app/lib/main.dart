10ff1e3b86fb325ca89098d6e250f169d0caacd1
dungngminh <63831488+dungngminh@users.noreply.github.com>
2022-07-02 14:56:00 +0700

    feat(mobile_app): finish mobile app

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,115 +1,405 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mobile app',
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  final Map<int, String> screenTypeData = {0: 'LCD', 1: 'OLED'};
+  final Map<int, String> osData = {0: 'Android', 1: 'iOS'};
+  final Map<int, String> chipsetData = {
+    0: 'Apple bionic',
+    1: 'Exynos',
+    2: 'Mediatek',
+    3: 'Snapdragon'
+  };
+  final Map<int, String> networkData = {4: '4G', 5: '5G'};
+  final Map<int, String> resolutionData = {
+    307200: 'VGA',
+    921600: 'HD',
+    2073600: 'Full HD',
+    3686400: '2K'
+  };
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  int? screenType;
+  int? chipSet;
+  int? os;
+  int? mobileNetwork;
+  int? screenResolution;
+  int ram = 0;
+  int rom = 0;
+  int battery = 0;
+  double price = 0.0;
+  String errorMessage = '';
+
+  final _formKey = GlobalKey<FormState>();
+  final _formatter = NumberFormat.simpleCurrency(locale: 'vi', name: 'đ');
+
+  Future<double> predictPrice() async {
+    final response = await http.Client().post(
+      Uri.parse('https://mobile-price-predict.azurewebsites.net/predict/'),
+      body: jsonEncode(<String, dynamic>{
+        'scr_type': screenType,
+        'chip': chipSet,
+        'OS': os,
+        'RAM': ram * 1024,
+        'ROM': rom * 1024,
+        'scr_res': screenResolution,
+        'battery': battery,
+        'net': mobileNetwork,
+      }),
+      headers: <String, String>{'Content-Type': 'application/json'},
+    );
+    if (response.statusCode != 200) {
+      throw Exception('Có lỗi xảy ra vui lòng thử lại');
+    }
+    return jsonDecode(response.body)['price'] * 1000;
+  }
+
+  Future<void> predict(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      FocusManager.instance.primaryFocus?.unfocus();
+
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Dự đoán giá'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FutureBuilder<double>(
+                  future: predictPrice(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    } else if (snapshot.hasError) {
+                      return Center(
+                        child: Text(snapshot.error as String),
+                      );
+                    } else {
+                      return Center(
+                        child: Text.rich(
+                          TextSpan(
+                            text: 'Giá dự đoán là: ',
+                            children: [
+                              TextSpan(
+                                text: _formatter.format(snapshot.data),
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              )
+                            ],
+                          ),
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(
+                child: const Text('Đóng'),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  void resetForm() {
+    FocusManager.instance.primaryFocus?.unfocus();
+    _formKey.currentState!.reset();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Dự đoán giá điện thoại',
+          ),
+        ),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.only(
+            top: 16,
+            left: 16,
+            right: 16,
+          ),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                DropdownButtonFormField<String>(
+                  onChanged: (value) {
+                    setState(() {
+                      screenType = int.parse(value ?? '0');
+                    });
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Screen type',
+                    border: OutlineInputBorder(),
+                  ),
+                  items: screenTypeData.entries
+                      .map<DropdownMenuItem<String>>(
+                        (e) => DropdownMenuItem(
+                          value: e.key.toString(),
+                          child: Text(e.value),
+                        ),
+                      )
+                      .toList(),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                DropdownButtonFormField<String>(
+                  onChanged: (value) {
+                    setState(() {
+                      os = int.parse(value ?? '0');
+                    });
+                  },
+                  validator: (value) {
+                    return value?.isEmpty == true
+                        ? 'Không được để trống trường này'
+                        : null;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'OS',
+                    border: OutlineInputBorder(),
+                  ),
+                  items: osData.entries
+                      .map<DropdownMenuItem<String>>(
+                        (e) => DropdownMenuItem(
+                          value: e.key.toString(),
+                          child: Text(e.value),
+                        ),
+                      )
+                      .toList(),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                DropdownButtonFormField<String>(
+                  onChanged: (value) {
+                    setState(() {
+                      chipSet = int.parse(value ?? '0');
+                    });
+                  },
+                  validator: (value) {
+                    return value?.isEmpty == true
+                        ? 'Không được để trống trường này'
+                        : null;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Chipset',
+                    border: OutlineInputBorder(),
+                  ),
+                  items: chipsetData.entries
+                      .map<DropdownMenuItem<String>>(
+                        (e) => DropdownMenuItem(
+                          value: e.key.toString(),
+                          child: Text(e.value),
+                        ),
+                      )
+                      .toList(),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                DropdownButtonFormField<String>(
+                  onChanged: (value) {
+                    setState(() {
+                      mobileNetwork = int.parse(value ?? '0');
+                    });
+                  },
+                  validator: (value) {
+                    return value?.isEmpty == true
+                        ? 'Không được để trống trường này'
+                        : null;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Mobile Network',
+                    border: OutlineInputBorder(),
+                  ),
+                  items: networkData.entries
+                      .map<DropdownMenuItem<String>>(
+                        (e) => DropdownMenuItem(
+                          value: e.key.toString(),
+                          child: Text(e.value),
+                        ),
+                      )
+                      .toList(),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                DropdownButtonFormField<String>(
+                  onChanged: (value) {
+                    setState(() {
+                      screenResolution = int.parse(value ?? '0');
+                    });
+                  },
+                  validator: (value) {
+                    return value?.isEmpty == true
+                        ? 'Không được để trống trường này'
+                        : null;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Screen Resolution',
+                    border: OutlineInputBorder(),
+                  ),
+                  items: resolutionData.entries
+                      .map<DropdownMenuItem<String>>(
+                        (e) => DropdownMenuItem(
+                          value: e.key.toString(),
+                          child: Text(e.value),
+                        ),
+                      )
+                      .toList(),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextFormField(
+                  onChanged: (value) {
+                    if (value.isNotEmpty) {
+                      setState(() {
+                        ram = int.parse(value);
+                      });
+                    }
+                  },
+                  validator: (value) {
+                    return value?.isEmpty == true
+                        ? 'Không được để trống trường này'
+                        : null;
+                  },
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: 'RAM',
+                    border: OutlineInputBorder(),
+                    suffixText: 'GB',
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextFormField(
+                  onChanged: (value) {
+                    if (value.isNotEmpty) {
+                      setState(() {
+                        rom = int.parse(value);
+                      });
+                    }
+                  },
+                  validator: (value) {
+                    return value?.isEmpty == true
+                        ? 'Không được để trống trường này'
+                        : null;
+                  },
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: 'ROM',
+                    border: OutlineInputBorder(),
+                    suffixText: 'GB',
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextFormField(
+                  onChanged: (value) {
+                    if (value.isNotEmpty) {
+                      setState(() {
+                        battery = int.parse(value);
+                      });
+                    }
+                  },
+                  validator: (value) {
+                    return value?.isEmpty == true
+                        ? 'Không được để trống trường này'
+                        : null;
+                  },
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: 'Battery',
+                    border: OutlineInputBorder(),
+                    suffixText: 'mAh',
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.green.shade400,
+                        onPrimary: Colors.white,
+                      ),
+                      onPressed: () async {
+                        await predict(context);
+                      },
+                      child: const Text('Dự đoán'),
+                    ),
+                    const SizedBox(
+                      width: 32,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.red.shade400,
+                        onPrimary: Colors.white,
+                      ),
+                      onPressed: resetForm,
+                      child: const Text('Reset'),
+                    )
+                  ],
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
